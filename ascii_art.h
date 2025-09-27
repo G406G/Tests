563d3ed5ee4eedcf8270b50ea10032902f6eee3b
G406G <pedrosorianoortiz29@gmail.com>
2025-09-27 14:27:47 -0400

    ascii_art.h

---
+++ ascii_art.h
@@ -0,0 +1,11 @@
+#ifndef ASCII_ART_H
+#define ASCII_ART_H
+
+void print_banner();
+void print_attack_header();
+void print_loading(int seconds);
+void print_success();
+void print_error();
+void print_stats_header();
+
+#endif
